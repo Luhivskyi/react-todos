15c1de4505ead63dc2bf9dce0e2cd5ef8befbe30
Luhivskyi <luhivskyi.d@ukr.net>
2021-11-17 09:37:59 +0300

    add todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import shortid from "shortid";
 import TodoList from "./components/TodoList";
+import TodoEditor from "./components/TodoEditor/TodoEditor";
+import Filter from "./components/Filter/Filter";
 import inosialTodos from "./todos.json";
 
 class App extends Component {
   state = {
     todos: inosialTodos,
+    filter: "",
+  };
+
+  addTodo = (text) => {
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    };
+
+    this.setState(({ todos }) => ({
+      todos: [todo, ...todos],
+    }));
   };
 
   deleteTodo = (todoId) => {
@@ -13,24 +29,69 @@ class App extends Component {
     }));
   };
 
-  render() {
+  toggleCompleted = (todoId) => {
+    // this.setState((prevState) => ({
+    //   todos: prevState.todos.map((todo) => {
+    //     if (todo.id === todoId) {
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed,
+    //       };
+    //     }
+
+    //     return todo;
+    //   }),
+    // }));
+
+    this.setState(({ todos }) => ({
+      todos: todos.map((todo) =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+      ),
+    }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleTodos = () => {
+    const { filter, todos } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return todos.filter((todo) =>
+      todo.text.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  calculateCompletedTodos = () => {
     const { todos } = this.state;
 
-    const totalTodoCount = todos.length;
-    const completedTodoCount = todos.reduce(
+    return todos.reduce(
       (total, todo) => (todo.completed ? total + 1 : total),
       0
     );
+  };
+
+  render() {
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculateCompletedTodos();
+    const visibleTodos = this.getVisibleTodos();
 
     return (
       <>
+        <TodoEditor onSubmit={this.addTodo} />
         <h1>Состояние компонента</h1>
         <div>
           <p>Общее кол-во: {totalTodoCount}</p>
           <p>Кол-во выполненных: {completedTodoCount}</p>
         </div>
-
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        <Filter value={filter} onChange={this.changeFilter} />
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
       </>
     );
   }
